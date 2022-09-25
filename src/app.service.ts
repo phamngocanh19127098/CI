56102bfca167951fa5ebaa442eb3294f47967614
phamngocanh19127098 <npham4533@gmail.com>
2022-09-25 20:17:12 +0700

    final

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'From the lastest version';
+    return 'Pham Ngoc Anh 19127098';
   }
 }
