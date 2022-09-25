717cc3729e3677147c60ad5e11b4df2655d04fb8
phamngocanh19127098 <npham4533@gmail.com>
2022-09-25 21:54:28 +0700

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Pham Ngoc Anh 19127098';
+    return 'Pham Ngoc Anh 19127098 HCMUS';
   }
 }
